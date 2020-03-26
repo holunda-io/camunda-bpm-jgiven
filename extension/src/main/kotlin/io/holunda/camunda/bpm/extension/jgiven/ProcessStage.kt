c659817d3a02c8ef1bdf29f5e908487788788e4f
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-03-26 12:12:42 +0100

    docs: add javadoc

--- extension/src/main/kotlin/io/holunda/camunda/bpm/extension/jgiven/ProcessStage.kt
+++ extension/src/main/kotlin/io/holunda/camunda/bpm/extension/jgiven/ProcessStage.kt
@@ -49,18 +49,34 @@ annotation class JGivenProcessStage
 @JGivenProcessStage
 class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Supplier<ProcessInstance>> : Stage<SELF>() {
 
+    /**
+     * Process engine to work on.
+     */
     @ExpectedScenarioState(required = true)
     lateinit var camunda: ProcessEngineRule
 
+    /**
+     * Instance supplier.
+     */
     @ScenarioState
     lateinit var processInstanceSupplier: PROCESS_BEAN
 
+    /**
+     * Checks if the process instance is waiting in a activity with specified id.
+     * @param activityId definition id.
+     * @return fluent stage.
+     */
     @As("process waits in $")
     fun process_waits_in(@Quoted activityId: String): SELF {
         assertThat(processInstanceSupplier.get()).isWaitingAt(activityId)
         return self()
     }
 
+    /**
+     * Asserts that the process is deployed.
+     * @param processDefinitionKey process definition key.
+     * @return fluent stage.
+     */
     @As("process $ is deployed")
     fun process_is_deployed(@Quoted processDefinitionKey: String): SELF {
         assertThat(camunda.repositoryService
@@ -71,46 +87,85 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts that the process is finished.
+     * @return fluent stage.
+     */
     fun process_is_finished(): SELF {
         assertThat(processInstanceSupplier.get()).isEnded
         return self()
     }
 
-    fun task_is_assigned_to_user(@Quoted user: String): SELF {
-        assertThat(task()).isAssignedTo(user)
-        return self()
-    }
-
-    fun task_is_visible_to_users(@QuotedVarargs users: Array<String>): SELF {
-        val task = task()
-        Arrays.stream(users).forEach { user -> assertThat(task).hasCandidateUser(user) }
-        return self()
-    }
-
-    fun task_is_visible_to_groups(@QuotedVarargs groups: Array<String>): SELF {
-        val task = task()
-        Arrays.stream(groups).forEach { group -> assertThat(task).hasCandidateGroup(group) }
-        return self()
-    }
-
+    /**
+     * Asserts that the process has passed in order elements.
+     * @param elements element definition ids.
+     * @return fluent stage.
+     */
     @As("process has passed element(s) $")
     fun process_has_passed(@QuotedVarargs vararg elements: String): SELF {
         assertThat(processInstanceSupplier.get()).hasPassedInOrder(*elements)
         return self()
     }
 
+    /**
+     * Asserts that the process has not passed elements.
+     * @param elements element definition ids.
+     * @return fluent stage.
+     */
     @As("process has not passed element(s) $")
     fun process_has_not_passed(@QuotedVarargs vararg elements: String): SELF {
         assertThat(processInstanceSupplier.get()).hasNotPassed(*elements)
         return self()
     }
 
+    /**
+     * Asserts that a task is assigned to a user.
+     * @param user username of the assignee.
+     * @return fluent stage.
+     */
+    fun task_is_assigned_to_user(@Quoted user: String): SELF {
+        assertThat(task()).isAssignedTo(user)
+        return self()
+    }
+
+    /**
+     * Asserts that a task is visible to user.
+     * @param users list of candidate users that must be candidate.
+     * @return fluent stage.
+     */
+    fun task_is_visible_to_users(@QuotedVarargs users: Array<String>): SELF {
+        val task = task()
+        Arrays.stream(users).forEach { user -> assertThat(task).hasCandidateUser(user) }
+        return self()
+    }
+
+    /**
+     * Asserts that a task is visible to groups.
+     * @param groups list of candidate groups that must be candidate.
+     * @return fluent stage.
+     */
+    fun task_is_visible_to_groups(@QuotedVarargs groups: Array<String>): SELF {
+        val task = task()
+        Arrays.stream(groups).forEach { group -> assertThat(task).hasCandidateGroup(group) }
+        return self()
+    }
+
+    /**
+     * Asserts the task task has a follow-up date
+     * @param followUpDatePeriod period calculated from creation.
+     * @return fluent stage.
+     */
     @As("task's follow-up date is $ after its creation")
     fun task_has_follow_up_date_after(followUpDatePeriod: Period): SELF {
         assertThat(task().followUpDate).isInSameSecondWindowAs(Date.from(task().createTime.toInstant().plus(followUpDatePeriod)))
         return self()
     }
 
+    /**
+     * Asserts task priority.
+     * @param priority specified priority.
+     * @return fluent stage.
+     */
     @As("task's priority is $")
     fun task_has_priority(priority: Int): SELF {
         assertThat(task().priority)
@@ -119,6 +174,11 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts priority.
+     * @param priority not the priority.
+     * @return fluent stage.
+     */
     @As("task's priority is not $")
     fun task_has_priority_other_than(priority: Int): SELF {
         assertThat(task().priority)
@@ -127,6 +187,11 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts priority.
+     * @param priority lower bound.
+     * @return fluent stage.
+     */
     @As("task's priority is greater than $")
     fun task_has_priority_greater_than(priority: Int): SELF {
         assertThat(task().priority)
@@ -135,6 +200,11 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts priority.
+     * @param priority upper bound.
+     * @return fluent stage.
+     */
     @As("task's priority is less than $")
     fun task_has_priority_less_than(priority: Int): SELF {
         assertThat(task().priority)
@@ -143,6 +213,12 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts priority.
+     * @param lower lower bound.
+     * @param upper upper bound.
+     * @return fluent stage.
+     */
     @As("task's priority is between \$lower and \$upper")
     fun task_priority_is_between(lower: Int = 0, upper: Int = 100): SELF {
         assertThat(task().priority)
@@ -151,6 +227,12 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts that variable is set.
+     * @param variableName name of the variable.
+     * @param value value of the variable.
+     * @return fluent stage.
+     */
     @As("variable \$variableName is set to \$value")
     fun variable_is_set(@Quoted variableName: String, @Quoted value: Any): SELF {
         assertThat(processInstanceSupplier.get()).hasVariables(variableName)
@@ -158,6 +240,11 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts that variable is not set.
+     * @param variableName name of the variable.
+     * @return fluent stage.
+     */
     @As("variables $ are not present")
     fun variable_is_not_present(@QuotedVarargs vararg variableName: String): SELF {
         assertThat(processInstanceSupplier.get())
@@ -171,6 +258,7 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
      * @param variables optional map with variables
      * @param continueIfAsync if <code>true</code> expects that the task is marked as async-after and continues the execution
      * after completion. Defaults to <code>false</code>.
+     * @return fluent stage.
      */
     @As("task is completed with variables $")
     fun task_is_completed_with_variables(@VariableMapFormat variables: VariableMap = createVariables(),
@@ -186,17 +274,29 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * No op.
+     * @return fluent stage.
+     */
     fun no_job_is_executed(): SELF {
         // empty
         return self()
     }
 
+    /**
+     * Executes current job.
+     * @return fluent stage.
+     */
     fun job_is_executed(): SELF {
         assertThat(processInstanceSupplier.get()).isNotNull
         execute(job())
         return self()
     }
 
+    /**
+     * Executes current job.
+     * @return fluent stage.
+     */
     @As("process continues")
     fun process_continues(): SELF {
         assertThat(processInstanceSupplier.get()).isNotNull
@@ -204,6 +304,11 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Asserts that external task exists.
+     * @param topicName name of the topic.
+     * @return fluent stage.
+     */
     @As("external task exists on topic \$topicName")
     fun external_task_exists(@Quoted topicName: String): SELF {
         val externalTasks = camunda
@@ -215,6 +320,13 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Completes external task.
+     * @param topicName name of the topic.
+     * @param variables variables to set on completion.
+     * @param continueIfAsync executes the async job after completion.
+     * @return fluent stage. 
+     */
     @As("external task on topic \$topicName is completed with variables \$variables")
     fun external_task_is_completed(@Quoted topicName: String, @VariableMapFormat variables: VariableMap = createVariables(), continueIfAsync: Boolean = false): SELF {
         camunda
@@ -236,6 +348,12 @@ class ProcessStage<SELF : ProcessStage<SELF, PROCESS_BEAN>, PROCESS_BEAN : Suppl
         return self()
     }
 
+    /**
+     * Correlates message.
+     * @param messageName name of the message.
+     * @param variables variables to set on correlation.
+     * @return fluent stage.
+     */
     @As("message \$messageName is received setting variables \$variables")
     fun message_is_received(@Quoted messageName: String, variables: VariableMap = createVariables()): SELF {
 
